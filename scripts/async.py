7e6f089f2885cc607ec297cac24b766a761091f9
Tristan <info@tristansiebers.nl>
2015-12-28 19:29:04 +0100

    Make threads daemons and implement a general runner

--- scripts/async.py
+++ scripts/async.py
@@ -32,3 +32,23 @@ def run_async(func):
         return func_hl
 
     return async_func
+
+
+def run_async_daemon(func):
+    """
+    run_async(func)
+        function decorator, intended to make "func" run in a separate
+        thread (asynchronously) and as a daemon.
+        Returns the created Thread object.
+    """
+    from threading import Thread
+    from functools import wraps
+
+    @wraps(func)
+    def async_func(*args, **kwargs):
+        func_hl = Thread(target=func, args=args, kwargs=kwargs)
+        func_hl.daemon = True
+        func_hl.start()
+        return func_hl
+
+    return async_func
