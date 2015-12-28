20a30f116ded4137c0e49a68daeac4cc5246a2e0
Tristan <info@tristansiebers.nl>
2015-12-28 22:55:04 +0100

    Fix runner

--- run.py
+++ run.py
@@ -13,10 +13,8 @@ wakeup = WakeUp.WakeUp(led_bar, 'wakeup.slpi')
 movement_detection = MovementDetection.MovementDetection(led_bar)
 
 
-
 def main_loop():
-    while 1:
-        # do your stuff... and wait a second.
+    while True:
         wakeup.check()
         time.sleep(0.5)
 
@@ -24,8 +22,7 @@ if __name__ == '__main__':
     try:
         # Start the movement detection.
         movement_loop = movement_detection.detect_movement(True)
-        wakeup.set_datetime('20151228', '205700')
-        wakeup.set_datetime('20151228', '205701')
+        wakeup.remove_old_items()
         main_loop()
     except KeyboardInterrupt:
         print >> sys.stderr, '\nExiting by user request.\n'
